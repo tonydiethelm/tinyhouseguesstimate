c70761dc8e55287cfefb8aad9a2ac24c04a1d49d
tonydiethelm <tonydiethelm@gmail.com>
2024-05-29 21:46:33 -0700

    fleshing out comments before starting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,65 @@
 </style>
 
 <script>
+/*
+Calculate: 
+2x4BoardFoot cost = cost of stud / length of stud in feet.
+2x6BoardFoot cost = 2x4BoardFoot cost * 6/4
+number of studs = (2L+2W)*12/16+4
+cost of studs = number of studs*cost of stud
+number of floor joists = length*12/16+1
+cost of floor joists = number of floor joists*width*2x6BoardFootCost. 
+number of roof joists = number of floor joists + 2
+cost of roof joists = number of floor joists*(width+2)*2x6BoardFootCost
+
+
+
+*/
+
+
+
 
 
 </script>
+
+
+<!--
+use pre filled out forms to gather needed data to do cals. 
+Do calcs
+spit out guesstimate
+
+
+Do a floating price guesstimate in the lower right corner
+$?
+
+Intro text
+We are assuming standard framing, 16"OC. We are assuming just one floor. 
+--
+We need some basic info about your Tiny House. 
+Trailer cost: 
+Length:
+width:
+Number of windows:
+--
+We need some basic info about the cost of things. 
+Cost of a 2x4:
+Cost of a piece of plywood for sheathing:
+Cost of water barrier:
+Cost of insulation:
+Cost of roofing:
+Cost of a window:
+Cost of a door:
+Cost of a shower:
+Cost of cabinets:
+Cost of a sink:
+Cost of an electrical panel:
+Cost of 12g wire:
+Cost of flooring:
+Cost of drywall:
+
+-->
+
+
 
 <h1>Welcome to the Tiny House Cost Guesstimator</h1>
 
